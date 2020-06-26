ec98517c4fdffd86a885b1d80e2148ffacb7d4e7
pushpraj4131 <pushpraj4131@gmail.com>
2020-06-26 18:38:50 +0530

    Changed cron job time

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.get('/constant-call', (req,res)=>{
 
 
 /*Cron job*/
-crontab.scheduleJob("0 0 * * 1-6" , function(){
+crontab.scheduleJob("30 18 * * 1-6" , function(){
 	console.log("hello");
 	// console.log("req . body ===>" , req.body.branch);
 	var indiaTime = momentTimeZone().tz("Asia/Kolkata").format()
